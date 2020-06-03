4d6996acd8d219489e2b5ffe11b11b9ee21a51b9
Chris Sanchez <chrisvsanchez@gmail.com>
2020-06-03 16:45:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,23 @@
 def using_push(array, string)
   array.push(string)\
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+  
+end
+def method_name
+  
+end
+def method_name
+  
+end
+
+def method_name
+  
+end
+def method_name
+  
+end
+
+
